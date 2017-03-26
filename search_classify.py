feb25cad19bfd4d3428375eacb6f16e1f5c00ea9
Tariq Rafique <mrafique@salesforce.com>
2017-03-25 22:40:11 -0700

    wip

--- search_classify.py
+++ search_classify.py
@@ -115,9 +115,6 @@ def main():
         elif 'non-vehicles' in image:
             notcars.append(image)
     
-    print (cars)
-    print (notcars)
-    return
     # Reduce the sample size because
     # The quiz evaluator times out after 13s of CPU time
     sample_size = 500
@@ -125,13 +122,13 @@ def main():
     notcars = notcars[0:sample_size]
 
     # TODO: Tweak these parameters and see how the results change.
-    color_space = 'RGB'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-    orient = 9  # HOG orientations
+    color_space = 'HLS'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    orient = 12  # HOG orientations
     pix_per_cell = 8  # HOG pixels per cell
     cell_per_block = 2  # HOG cells per block
-    hog_channel = 0  # Can be 0, 1, 2, or "ALL"
+    hog_channel = 2  # Can be 0, 1, 2, or "ALL"
     spatial_size = (16, 16)  # Spatial binning dimensions
-    hist_bins = 16    # Number of histogram bins
+    hist_bins = 48    # Number of histogram bins
     spatial_feat = True  # Spatial features on or off
     hist_feat = True  # Histogram features on or off
     hog_feat = True  # HOG features on or off
@@ -180,7 +177,7 @@ def main():
     # Check the prediction time for a single sample
     t = time.time()
 
-    image = mpimg.imread('bbox-example-image.jpg')
+    image = mpimg.imread('sample/bbox-example-image.jpg')
     draw_image = np.copy(image)
 
     # Uncomment the following line if you extracted training
