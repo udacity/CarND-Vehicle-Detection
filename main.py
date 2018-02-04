7e133cd1566abe89bf55b9b23e38f172927b1113
Dongmin Han <dongminhan@gatech.com>
2018-02-04 01:39:04 -0500

    added main.py

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
-import cv2
+from lesson_functions import *
+import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture('test_video.mp4')
+cap = cv2.VideoCapture('project_video.mp4')
+# create output video
+out = cv2.VideoWriter('project_result.mp4',fourcc, 25.0, (640,480))
 
 # Check if camera opened successfully
 if cap.isOpened() == False:
     print("Error opening video stream or file")
 
-# Read until video is completed
 numFrame = 0
 while cap.isOpened():
     # Capture frame-by-frame
     ret, frame = cap.read()
     if ret == True:
-
+        numFrame += 1
         # Display the resulting frame
         cv2.imshow('Frame', frame)
-        # Save img as .png
-        numFrame += 1
-        cv2.imwrite('./train_images/successive_frames/frame' + str(numFrame) + '.png', frame)
+
+        # get box list for current frame
+        out_img,box_list = find_cars()
+
+        if numFrame <= 10:
+            # add box_list to list
+        else:
+            # delete old list, add new list, apply threshold
+
+            # draw_labeled_bboxes
+
+            # save current frame
+            out.write(xxx)
         # Press Q on keyboard to  exit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
@@ -31,6 +45,6 @@ while cap.isOpened():
 
 # When everything done, release the video capture object
 cap.release()
-
+out.release()
 # Closes all the frames
 cv2.destroyAllWindows()
